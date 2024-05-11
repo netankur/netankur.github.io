6df199fb16356cab899a076345754b5b669d4d5c
ankurdxx <ankurdas0102@gmail.com>
2024-05-11 07:34:01 +0530

    Update app.js

--- home/app.js
+++ home/app.js
@@ -1,6 +1,6 @@
 if ('serviceWorker' in navigator) {
     window.addEventListener('load', function() {
-        navigator.serviceWorker.register('service-worker.js')
+        navigator.serviceWorker.register('/home/service-worker.js')
         .then(function(registration) {
             console.log('Service Worker registered with scope:', registration.scope);
         }, function(err) {
